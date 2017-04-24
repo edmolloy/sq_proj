a18c86ec2be33731edb8fbcddd8c6ee430febb99
ggallagher108 <gk7579@uncw.edu>
2017-04-24 11:40:19 -0400

    Added comments

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.app') <!-- Import Header and Css information -->
 
-@section('content')
+@section('content') <!-- Start module -->
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -9,9 +9,9 @@
 
                 <div class="panel-body">
                     You are logged in!
-                </div>
+                </div> 
             </div>
         </div>
     </div>
 </div>
-@endsection
+@endsection <!-- End module -->
